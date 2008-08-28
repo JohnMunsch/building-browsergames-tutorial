1341b5d3cb9d9b71dd6eb1859bf60e69a438dbf7
john.munsch <john.munsch@8c67c1ad-f553-0410-b016-77e945ac5e8b>
2008-08-28 12:09:12 +0000

    Added a welcome message to the user for the templating entry which will be posted within a few days.

--- trunk/rubyonrails/pbbg/app/controllers/welcome_controller.rb
+++ trunk/rubyonrails/pbbg/app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    # Call the function current_user to get the user assigned to the 
+    # @current_user variable (if anyone is logged in). The
+    # function comes with restful_authentication.
+    current_user
   end
-
 end
