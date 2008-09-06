431baed5fe4321e9c571fc7cd0cfc9b395f3531a
john.munsch <john.munsch@8c67c1ad-f553-0410-b016-77e945ac5e8b>
2008-09-06 19:14:29 +0000

    Added combat to the system. You can now go into the forest, encounter a monster, and attack or run away.

--- rubyonrails/pbbg/app/controllers/forest_controller.rb
+++ rubyonrails/pbbg/app/controllers/forest_controller.rb
@@ -1,16 +1,32 @@
 class ForestController < ApplicationController
-  # We don't want anyone other than logged in users going to the forest.
+  # We don't want anyone other than logged in users going to the forest or executing
+  # actions inside the forest.
   before_filter :login_required
   
   def index
     current_user
     
+    # Get a random monster for us to encounter in the forest.
     @monster = Monster.random_encounter
+    
+    # Save the monster in our session.
+    session[:monster_id] = @monster.id
   end
   
   def attack
+    current_user
+    
+    # Pull the monster we were fighting from the session.
+    @monster = Monster.find(session[:monster_id])
+    
+    # Fight the monster and get the transcript of the fight.
+    @combat = @current_user.fight(@monster)
   end
   
+  # Running away couldn't be much easier, just send the user back to the welcome page.
   def run_away
+    session[:monster_id] = nil
+    
+    redirect_to :controller => "welcome"
   end
 end
--- rubyonrails/pbbg/app/models/user.rb
+++ rubyonrails/pbbg/app/models/user.rb
@@ -38,6 +38,64 @@ class User < ActiveRecord::Base
     self.cur_hp > 0
   end
 
+  def fight(monster)
+    combat = Array.new
+    
+    turn_number = 0
+    
+    # This is a small workaround for the fact that we aren't getting the user's name yet
+    # when they signup. All we have for the user is the login.
+    if (self.name.empty?)
+      self.name = self.login
+    end
+    
+    while (self.alive? and monster.alive?)
+      turn_number += 1
+      
+      # Switch the roles of attacker and defender back and forth.
+      if (turn_number % 2 != 0)
+        attacker = monster
+        defender = self
+      else
+        attacker = self
+        defender = monster
+      end
+      
+      if (attacker.attack > defender.defense)
+        damage = attacker.attack - defender.defense
+        
+        # We allow damage to take you below zero hitpoints. Presumably the funeral is
+        # closed casket in those cases.
+        defender.cur_hp -= damage
+
+        # We'll only make a turn record for those cases where something actually happens.
+        turn = Hash.new
+        turn["attacker"] = attacker.name
+        turn["defender"] = defender.name
+        turn["damage"] = damage
+        
+        # Save this turn in the combat transcript.
+        combat << turn
+      end
+    end
+    
+    # Somebody is dead, let's see who and take appropriate action.
+    if (self.alive?)
+      # Yay, you lived. You get gold.
+      self.gold += monster.gold
+    else
+      # There aren't any penalties for losing at the moment.
+    end
+
+    # We only save the user's record back to the database. The monster needs to
+    # stay unchanged in the database no matter how much damage we did to it so
+    # the next one we make will still be pristine.
+    self.save
+    
+    # Return the results of combat.
+    combat
+  end
+
   protected
   
   def before_create
